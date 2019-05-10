95ca749fac1f3612f0e2b400afecf9f64056147b
suyanlong <592938410@qq.com>
2019-05-10 10:27:52 +0800

    Add ubuntut:16.04 Dockerfile

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+set -o verbose
+
+
+cat << EOF >> ${HOME}/.bash_profile
+export GOROOT=/opt/go
+export GOPATH=${HOME}/go
+export PATH=$GOROOT/bin:$GOPATH/bin:$PATH
+export GO111MODULE=on
+
+export RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static
+export RUSTUP_UPDATE_ROOT=https://mirrors.ustc.edu.cn/rust-static/rustup
+EOF
+
+RUN source ${HOME}/.bash_profile && go version && go env
+
+cat << EOF > $HOME/.cargo/config
+[source.crates-io]
+replace-with = 'ustc'
+
+[source.ustc]
+registry = "git://mirrors.ustc.edu.cn/crates.io-index"
+
+EOF
+
+# rust env
+RUN curl https://sh.rustup.rs -sSf >> rustup-init.sh \
+    && chmod +x rustup-init.sh \
+    && ./rustup-init.sh -y --no-modify-path \
+    && source $HOME/.cargo/env \
+    && rustup show \
+    && cargo version
+
+# tool
+RUN cargo install fd-find bat hyperfine
